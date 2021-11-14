8107052acd74261246f1c2cb9e0e6c57163db718
Oludare <oludare@pc-69-225.customer.ask4.lan>
2021-11-14 12:56:08 +0100

    test playlist updater cron

--- jobs/agenda.js
+++ jobs/agenda.js
@@ -67,7 +67,7 @@ agenda.define("update followers", async (job, done) => {
 //   console.log(job);
 // });
 
-if (process.env.NODE_ENV === "production")
+if (process.env.NODE_ENV === "deve")
   agenda.on("ready", async () => await agenda.start());
 
 module.exports = agenda;
