b660866741b0ee9ad735fa381f008279699b4c9e
Oludare <oludare@pc-69-225.customer.ask4.lan>
2021-11-14 12:48:19 +0100

    fix check followers cron job update

--- services/cronService.js
+++ services/cronService.js
@@ -1,12 +1,19 @@
+const { sub } = require("date-fns");
 const { exec } = require("child_process");
 const cron = require("node-cron");
 const Playlistfollower = require("../models/PlaylistFollowers");
 
-exports.schedulePlaylistFollowersCheck = () => {
+exports.schedulePlaylistFollowersCheck = async () => {
   console.log("scheduled database updates check");
-  cron.schedule("*/5 * * * *", async () => {
+  cron.schedule("*/1 * * * *", async () => {
     console.log("checking database for recent followers", new Date());
-    const followers = await Playlistfollower.find();
+    const followers = await Playlistfollower.find({
+      createdAt: {
+        $gte: sub(new Date(), { minutes: 5 }),
+        $lte: new Date(),
+      },
+    });
+    console.log(followers.length);
     if (followers.length < 1) {
       console.log("no followers found restarting server", new Date());
       exec("pm2 reload server");
