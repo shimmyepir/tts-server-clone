79835f7b5adaec707f0d5dd0811682da718810c4
dre@gmail.com <dre@gmail.com>
2022-07-02 10:33:35 +0200

    change percentage

--- services/cronService.js
+++ services/cronService.js
@@ -21,7 +21,7 @@ exports.schedulePlaylistFollowersCheck = async () => {
       .sort("-createdAt")
       .limit(1);
     console.log(followers.length, playlistCount[0].count);
-    if (followers.length < playlistCount[0].count * 0.5) {
+    if (followers.length < playlistCount[0].count * 0.1) {
       await new Email("udistribusiness@gmail.com").sendPlaylistNotTracking();
       await new Email("oludareodedoyin@gmail.com").sendPlaylistNotTracking();
       console.log("no followers found restarting server", new Date());
